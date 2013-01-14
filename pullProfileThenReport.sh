6403e7a6eed7433c1bf15dc231a75849d91eabfd
danieru <danieru.dressler@gmail.com>
2013-01-13 21:22:55 -0700

    Add profile report generation script

---
+++ pullProfileThenReport.sh
@@ -0,0 +1,2 @@
+adb pull /sdcard/gmon.out .
+../../.android-ndk/toolchains/arm-linux-androideabi-4.4.3/prebuilt/linux-x86/bin/arm-linux-androideabi-gprof obj/local/armeabi-v7a/libdesmumeneon.so > report.txt
